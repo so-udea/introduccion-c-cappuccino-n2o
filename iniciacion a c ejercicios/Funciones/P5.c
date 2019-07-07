76f7689a767d1ccd4d0169a327d0bf5596f17014
juan pablo <juan.jaramillo62@udea.edu.co>
2019-07-07 18:51:09 -0500

    el P5

---
+++ iniciacion a c ejercicios/Funciones/P5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int invertirValor(int valor);
+
+int main() {
+
+    int valor;
+    scanf("%d",&valor);
+    printf("valor de enntrada: %d \n", valor);
+    printf("valor invertido: %d", invertirvalor(valor));
+    scanf("%d",&valor);
+
+}
+
+int invertirValor(valor){
+
+    int res, valorInvertido;
+
+    while(valor>0){
+
+        res = valor%10;
+        valor=valor/10;
+        valorInvertido=valorInvertido*10+res;
+
+    }
+
+    return valorInvertido;
+}
